b6489b51a71af756447de6a8db602b352942b53e
Kirill Danilchuk <kirill.danilchuk01@gmail.com>
2022-04-22 22:34:00 +0300

    Append lab 4

--- program/database/database.hpp
+++ program/database/database.hpp
@@ -1,9 +1,18 @@
 #pragma once
 #include <string>
 
+/**
+ * @brief Класс для работы с базой данных
+ * @author Кирилл Данильчук
+ */
 class Database {
   Database() = default;
   bool Connect();
+  /**
+   * @brief Отправляет запрос в базу данных
+   * @param query Строка запроса
+   * @return Ответ от базы данных
+   */
   std::string Query(const std::string& query);
 
  private:
